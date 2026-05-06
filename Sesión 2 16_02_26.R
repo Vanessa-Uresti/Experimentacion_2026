f4d4343cf002ddc335fcbcb27a9724367602a3f1
Vanessa_Uresti <vanessaurestir@uanl.edu.mx>
2026-05-06 14:39:01 -0600

    act

--- Sesión 2 16_02_26.R
+++ Sesión 2 16_02_26.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Vanessa M Uresti Rdidriguez 
 # Matricula: 2076842
 # Maestria en Ciencias Forestales 
@@ -51,58 +50,4 @@ hist(IE$IE,
      ylim = c(0,12),
      main = "",
      ylab ="Frecuencia",
-=======
-# Vanessa M Uresti Rdidriguez 
-# Matricula: 2076842
-# Maestria en Ciencias Forestales 
-# 16/02/2025
-# sesión 2
-# Importar datos --------------------------------------------------------
-
-# Función read.csv
-
-IE <-read.csv("01_datos/vivero.csv", header = T)
-IE$Tratamiento <-as.factor(IE$Tratamiento)
-
-View(IE)
-
-tapply(IE$IE, IE$Tratamiento, mean)
-tapply(IE$IE, IE$Tratamiento, sd)
-tapply(IE$IE, IE$Tratamiento, var)
-
-
-# Diseño de gráfico -------------------------------------------------------
-
-
-boxplot(IE$IE)
-
-boxplot(IE$IE ~ IE$Tratamiento,
-        col = "lightblue",
-        xlab = "Fertilizante",
-        ylab = "IE",
-        main = "Vivero FCF",
-        ylim = c(0.4, 1.2))
-# El símbolo ~ se utiliza para indicar que una variable depende de otra,
-# especialmente cuando se trabaja con varias variables dentro de un modelo.
-
-# Para prevenir errores al compilar, es importante revisar bien la carpeta
-# de trabajo. Después de guardar nuevamente el archivo, ya se puede compilar.
-
-
-# Pruebas de normalida ----------------------------------------------------
-#Safiro y Kormogorof
-
-shapiro.test(IE$IE)
-
-# Homogeneidad de varianza ------------------------------------------------
-
-bartlett.test(IE$IE ~ IE$Tratamiento)
-
-
-hist(IE$IE,
-     col = "blue",
-     ylim = c(0,12),
-     main = "",
-     ylab ="Frecuencia",
->>>>>>> 35e2b24b3d23f815ab07cdd8088b17a2ef220dbf
-     xlab = "Variable IE")
+)
